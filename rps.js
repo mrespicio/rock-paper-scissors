934129c69055d0b0961f130eca7081b6d8ec0437
mrespicio <meganrespicio@hotmail.com>
2022-12-20 18:51:49 -0800

    create functions to get player and cpu choices

--- rps.js
+++ rps.js
@@ -1,1 +1,41 @@
 console.log("Hello World!");
+
+
+function getComputerChoice(){
+	let choice;
+	let choiceRandomized = Math.floor(Math.random() * 3)
+	switch (choiceRandomized){
+		case 0:
+			choice = "rock";
+			break;
+		case 1:
+			choice = "paper";
+			break;
+		case 2:
+			choice = "scissors";
+			break;
+		}
+	return choice;
+}
+
+function getPlayerChoice(){
+	let input = prompt().toLowerCase();
+	return input;
+}
+
+function playRound(playerSelection, computerSelection){
+	playerSelection = playerSelection.toLowerCase();
+
+	console.log("Computer choice: " + computerSelection)
+	console.log("Player choice: " + playerSelection)
+	if(playerSelection === computerSelection) console.log("Tie!")
+	else console.log("Not a tie")
+}
+
+
+
+playRound(getPlayerChoice(), getComputerChoice());
+
+
+
+
